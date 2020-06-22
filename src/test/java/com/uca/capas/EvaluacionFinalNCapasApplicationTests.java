e18067a9c8b617d48d0f282839b7390f4ffd3835
Paola Dominguez <paoladomdiaz@gmail.com>
2020-06-22 09:56:20 -0600

    first commit

---
+++ src/test/java/com/uca/capas/EvaluacionFinalNCapasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uca.capas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvaluacionFinalNCapasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
