cb2ef524e59a765508401c2ca84ead958569f509
Paola Dominguez <paoladomdiaz@gmail.com>
2020-07-10 01:49:20 -0600

    script de base de datos

--- bdd-backup/CentroEscolarScript.sql
+++ bdd-backup/CentroEscolarScript.sql
@@ -138,3 +138,17 @@ INSERT INTO CatalogoMaterias(nombre, estado)
 
 INSERT INTO "alumno" (nombreestudiante,apellidoestudiante,carnetminoridad,fechanacimiento,edad,direccion,telefonofijo,telefonocelular,nombrepadre,nombremadre,idcentroescolar) VALUES ('Benjamin','Webster','1663062-4','2000/09/19',19,'2152 Ut, Road','22378300','70756751','Hayden Gibson','Kameko Madden',10),('Dalton','Randall','1609031-7','1996/06/10',27,'Ap #230-9457 Risus Rd.','21455701','76141160','Carson Blackburn','Adria Whitehead',12),('Xander','Woodard','1685043-3','2002/09/18',23,'2420 Diam Road','27812702','73018959','Armando Jordan','Althea Cole',15),('Cole','Solis','1635382-1','1991/07/19',19,'995 Tempus St.','29005613','71533314','Evan Holloway','Cassidy Cameron',15),('Jasmine','Castro','1644090-7','1997/10/13',16,'Ap #923-6777 Nullam Rd.','20219396','71012637','Abel Salazar','Colleen Rowe',6),('Nita','Palmer','1671102-2','1998/12/18',21,'1359 Parturient St.','24563662','74373336','Ryder Richmond','Karly Sherman',11),('Keegan','Thornton','1690018-7','1991/05/16',24,'5334 Odio. Ave','26984912','77539934','Edan Valentine','Leandra Reid',5),('Holly','Briggs','1635021-6','1996/06/23',15,'Ap #661-3341 At Av.','23842678','70377282','Malachi Macias','Madeson Sears',20),('Denton','Reid','1688011-9','1996/11/11',19,'429-4744 Aliquet Rd.','27954010','77209759','Ivan Roth','Daphne Flynn',5),('Tate','Estrada','1644072-9','1993/02/14',20,'P.O. Box 266, 8200 Integer St.','27715526','76952464','Lane Holman','Zorita Spears',6);
 INSERT INTO "alumno" (nombreestudiante,apellidoestudiante,carnetminoridad,fechanacimiento,edad,direccion,telefonofijo,telefonocelular,nombrepadre,nombremadre,idcentroescolar) VALUES ('Colorado','Rich','1641082-9','2000/10/03',24,'P.O. Box 344, 2197 Semper Road','27894086','70181905','Oren Gregory','Venus Hutchinson',13),('Amelia','Mcclure','1661061-6','1992/05/07',16,'369 In Road','23478802','77023411','Randall Gilliam','Simone Vazquez',6),('Nissim','Lopez','1692033-3','1997/04/02',28,'P.O. Box 412, 7798 Iaculis, Rd.','24057225','79204708','Ahmed Ortiz','Mechelle Farmer',14),('Herman','Kirby','1695052-0','1995/06/01',19,'1045 Eget Ave','22072718','74091710','Logan Mitchell','Velma Washington',13),('Hope','Porter','1636042-7','2004/12/05',27,'Ap #816-9822 Hendrerit Road','28062314','77533377','Nigel Albert','Kay Sellers',6),('Rae','Sweeney','1687012-7','2004/05/08',28,'894 Purus St.','22457672','72612621','Octavius Mcintyre','Irene Contreras',11),('Graiden','Johnston','1692080-6','1995/01/28',16,'4968 Sagittis Street','28661769','77464115','Clinton Heath','Kylee Bishop',18),('Andrew','Rosales','1612011-9','1998/09/07',25,'3739 Blandit Avenue','25332176','79141358','Preston Johns','Destiny Melton',11),('Jameson','Mccormick','1689022-9','1995/08/31',23,'P.O. Box 251, 5110 Vitae St.','27586001','79249208','Gareth Ortega','Deborah Castaneda',9),('Curran','Cooke','1677042-2','2000/05/01',18,'P.O. Box 149, 4372 Ligula. Av.','22345835','78504681','Clayton Fuentes','Eliana Mcgee',5);
+
+insert into "materia" (anio, ciclo, nota, resultado, idCatalogo,idAlumno) values (2018, 2, 9.0, 'APROBADO', 1, 1),
+(2019, 1, 4.0, 'REPROBADO', 4, 1),
+(2018, 2, 7.0, 'APROBADO', 9, 2),
+(2017, 2, 6.0, 'REPROBADO', 5, 2),
+(2017, 1, 7.0, 'APROBADO', 14, 3),
+(2018, 3, 9.0, 'APROBADO', 13, 3),
+(2018, 2, 7.0, 'APROBADO', 1, 4),
+(2010, 2, 8.0, 'APROBADO', 2, 4),
+(2012, 1, 5.5, 'REPROBADO', 7, 5),
+(2015, 2, 6.7, 'APROBADO', 5, 5),
+(2018, 2, 3.6, 'REPROBADO', 4, 6),
+(2019, 1, 6.6, 'APROBADO', 7, 6),
+(2018, 2, 5.0, 'REPROBADO', 6, 7);
